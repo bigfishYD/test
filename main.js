da9ea863fa33c3898ddf2c53834d152de180e23f
bigfish <bigfish@qq.com>
2021-02-18 03:40:03 +0800

    测试

---
+++ main.js
@@ -0,0 +1,20 @@
+var fs = require("fs");
+var events = require("events");
+
+var eventEmitter = new events.EventEmitter();
+var connectHandler = function () {
+    console.log("连接成功")
+
+    eventEmitter.emit("data_recevied");
+}
+
+    // 绑定 connection 事件处理程序
+    eventEmitter.on('connection', connectHandler);
+    eventEmitter.on("data_recevied",function () {
+        console.log("数据接收成功");
+    })
+
+
+    eventEmitter.emit("connection");
+    console.log("程序执行完毕。");
+
